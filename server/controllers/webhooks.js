ed685bb862d54161dd587e257f70f5f07eb930f9
sachin624k <sachin77000k@gmail.com>
2026-02-14 18:53:39 +0530

    correction

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -2,16 +2,16 @@ import { Webhook } from "svix";
 import User from "../models/User.js";
 
 // API Controller Funtion to Manage Clerk User with database
-export const clerkWebhooks = async(req, res) => {
+export const clerkWebhooks = async (req, res) => {
     try {
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRE)
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
         await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
         })
-        const {data, type} = req.body
+        const { data, type } = req.body
 
         switch (type) {
             case 'user.created': {
@@ -25,10 +25,10 @@ export const clerkWebhooks = async(req, res) => {
                 res.json({})
                 break
             }
-                
+
             case 'user.updated': {
                 const userData = {
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
@@ -42,12 +42,12 @@ export const clerkWebhooks = async(req, res) => {
                 res.json({})
                 break;
             }
-        
+
             default:
                 break;
         }
 
     } catch (error) {
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 } 
